dbe6b8ea0f3c63c354281ea03df5b143c681c063
erisdll <erika.melloramos@gmail.com>
2025-06-17 09:52:59 -0300

    refactor: rename user-service to customer-service and update package structure

--- services/customer-service/src/main/java/com/erika/springshop/customer/UserServiceApplication.java
+++ services/customer-service/src/main/java/com/erika/springshop/customer/UserServiceApplication.java
@@ -1,4 +1,4 @@
-package com.springshop.user;
+package com.erika.springshop.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
