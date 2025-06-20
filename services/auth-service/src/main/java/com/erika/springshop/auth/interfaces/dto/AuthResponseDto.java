ecedec6c1fae60785dcf88a88f57293a53001bba
erisdll <erika.melloramos@gmail.com>
2025-06-20 12:09:32 -0300

    feat: implement AuthResponseDto

---
+++ services/auth-service/src/main/java/com/erika/springshop/auth/interfaces/dto/AuthResponseDto.java
@@ -0,0 +1,6 @@
+package com.erika.springshop.auth.interfaces.dto;
+
+public record AuthResponseDto(String token,
+                              String username,
+                              String email
+) {}
