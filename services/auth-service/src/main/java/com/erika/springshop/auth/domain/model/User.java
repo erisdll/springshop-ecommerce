0992879f9c677e29118b74dabeaca0cbb3e9f514
erisdll <erika.melloramos@gmail.com>
2025-06-20 12:10:44 -0300

    feat: implement User entity with UserDetails integration and role management

--- services/auth-service/src/main/java/com/erika/springshop/auth/domain/model/User.java
+++ services/auth-service/src/main/java/com/erika/springshop/auth/domain/model/User.java
@@ -1,4 +1,49 @@
 package com.erika.springshop.auth.domain.model;
 
-public class User {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String username;
+
+    @Column
+    private String password;
+
+    @Column
+    private String role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() { return List.of(new SimpleGrantedAuthority(role)); }
+
+    @Override
+    public boolean isAccountNonExpired() { return UserDetails.super.isAccountNonExpired(); }
+
+    @Override
+    public boolean isAccountNonLocked() { return UserDetails.super.isAccountNonLocked(); }
+
+    @Override
+    public boolean isCredentialsNonExpired() { return UserDetails.super.isCredentialsNonExpired(); }
+
+    @Override
+    public boolean isEnabled() { return UserDetails.super.isEnabled(); }
 }
