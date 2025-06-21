82ed91cfe3bfa021bd85895d6742092ed2f8dcc1
erisdll <erika.melloramos@gmail.com>
2025-06-21 17:46:23 -0300

    feat: add authentication and registration endpoints with request and response DTOs

--- services/auth-service/src/main/java/com/erika/springshop/auth/interfaces/controller/AuthenticateUserController.java
+++ services/auth-service/src/main/java/com/erika/springshop/auth/interfaces/controller/AuthenticateUserController.java
@@ -1,6 +1,13 @@
 package com.erika.springshop.auth.interfaces.controller;
 
+import com.erika.springshop.auth.application.port.in.registercustomer.AuthenticateUserUseCase;
+import com.erika.springshop.auth.interfaces.dto.AuthRequestDto;
+import com.erika.springshop.auth.interfaces.dto.AuthResponseDto;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth/login")
 @RequiredArgsConstructor
 public class AuthenticateUserController {
+
+    private final AuthenticateUserUseCase authenticateUserUseCase;
+
+    @PostMapping
+    public ResponseEntity<AuthResponseDto> authenticateUser(@Valid @RequestBody AuthRequestDto authRequestDto) {
+        AuthResponseDto authResponseDto = authenticateUserUseCase.execute(authRequestDto);
+        return ResponseEntity.ok(authResponseDto);
+    }
+
 }
--- services/auth-service/src/main/java/com/erika/springshop/auth/interfaces/controller/RegisterUserController.java
+++ services/auth-service/src/main/java/com/erika/springshop/auth/interfaces/controller/RegisterUserController.java
@@ -1,6 +1,14 @@
 package com.erika.springshop.auth.interfaces.controller;
 
+import com.erika.springshop.auth.application.port.in.registercustomer.RegisterUserUseCase;
+import com.erika.springshop.auth.interfaces.dto.SignUpRequestDto;
+import com.erika.springshop.auth.interfaces.dto.SignUpResponseDto;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth/signup")
 @RequiredArgsConstructor
 public class RegisterUserController {
+
+    private final RegisterUserUseCase registerUserUseCase;
+
+    @PostMapping
+    public ResponseEntity<SignUpResponseDto> registerUser(@Valid @RequestBody SignUpRequestDto signUpRequestDto) {
+        SignUpResponseDto signUpResponseDto = registerUserUseCase.execute(signUpRequestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(signUpResponseDto);
+    }
 }
