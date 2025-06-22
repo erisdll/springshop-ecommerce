72c0d41526a2e2add426387027aa30dc5f8e71e8
erisdll <erika.melloramos@gmail.com>
2025-06-22 01:23:48 -0300

    feat: add bypass for authentication filter on /api/auth endpoints

--- services/auth-service/src/main/java/com/erika/springshop/auth/infrastructure/security/jwt/JwtAuthenticationFilter.java
+++ services/auth-service/src/main/java/com/erika/springshop/auth/infrastructure/security/jwt/JwtAuthenticationFilter.java
@@ -31,6 +31,13 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                                     FilterChain filterChain)
             throws ServletException, IOException {
 
+        String path = request.getServletPath();
+
+        if (path.startsWith("/api/auth")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         final String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
 
         if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
